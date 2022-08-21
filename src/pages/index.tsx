e7fddca7b894abac22587f2190ac980d7f056dc0
Albert Vila Calvo <vilacalvo.albert@gmail.com>
2022-08-21 20:48:48 +0200

    index.js - add DocIndex to de-clutter

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -81,6 +81,39 @@ function HomepageHeader() {
   )
 }
 
+function DocIndex() {
+  return (
+    <div className="container">
+      <div className={styles.grid3col}>
+        {Object.keys(sidebars)
+          .filter((sidebarName) => sidebarName !== 'docs')
+          .map((sidebarName) => (
+            <div key={sidebarName}>
+              <h2>{sidebarName}</h2>
+              <div>
+                {Array.isArray(sidebars[sidebarName]) &&
+                  sidebars[sidebarName].map((sidebarItem) =>
+                    typeof sidebarItem === 'string' ? (
+                      <DocLink key={sidebarItem} to={sidebarItem} />
+                    ) : typeof sidebarItem === 'object' ? (
+                      <div key={sidebarItem.label}>
+                        <h3>{sidebarItem.label}</h3>
+                        {sidebarItem.items.map((subItem) => (
+                          <DocLink key={subItem} to={subItem} />
+                        ))}
+                      </div>
+                    ) : (
+                      <p>Warning: unexpected item type</p>
+                    )
+                  )}
+              </div>
+            </div>
+          ))}
+      </div>
+    </div>
+  )
+}
+
 function DocLink({ to, ...props }: { to: string }) {
   return (
     <a href={to} {...props} className={styles.docLink}>
@@ -102,34 +135,7 @@ export default function Home(): JSX.Element {
     >
       <HomepageHeader />
       <main>
-        <div className="container">
-          <div className={styles.grid3col}>
-            {Object.keys(sidebars)
-              .filter((sidebarName) => sidebarName !== 'docs')
-              .map((sidebarName) => (
-                <div key={sidebarName}>
-                  <h2>{sidebarName}</h2>
-                  <div>
-                    {Array.isArray(sidebars[sidebarName]) &&
-                      sidebars[sidebarName].map((sidebarItem) =>
-                        typeof sidebarItem === 'string' ? (
-                          <DocLink key={sidebarItem} to={sidebarItem} />
-                        ) : typeof sidebarItem === 'object' ? (
-                          <div key={sidebarItem.label}>
-                            <h3>{sidebarItem.label}</h3>
-                            {sidebarItem.items.map((subItem) => (
-                              <DocLink key={subItem} to={subItem} />
-                            ))}
-                          </div>
-                        ) : (
-                          <p>Warning: unexpected item type</p>
-                        )
-                      )}
-                  </div>
-                </div>
-              ))}
-          </div>
-        </div>
+        <DocIndex />
         {/*
         {features && features.length > 0 && (
           <section className={styles.features}>
